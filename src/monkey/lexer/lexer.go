443841341637c0ec521ff484030d1baba05f051b
cfr2ak <cfr2ak@gmail.com>
2019-10-22 12:32:56 +0900

    Revert "Change directory structure"
    This reverts commit 81ac9e217a9478d98dd619ff053da943aca38439.

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -11,13 +11,3 @@ func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	return l
 }
-
-func (l *Lexer) readChar() {
-	if l.readPosition >= len(l.input) {
-		l.ch = 0
-	} else {
-		l.ch = l.input[l.readPosition]
-	}
-	l.position = l.readPosition
-	l.readPosition += 1
-}
